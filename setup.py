e9f47693512473733a96269020d58f2824cb8583
CrazyFox7048 <134632466+CrazyFox7048@users.noreply.github.com>
2023-11-06 14:32:11 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+import py2exe
+import tkinter, urllib.request, os, pathlib, tempfile, subprocess, webbrowser, time, shutil
+from PIL import Image, ImageTk
+setup(windows=['WSCAppStoreV1.1.py'], py_modules=[])
